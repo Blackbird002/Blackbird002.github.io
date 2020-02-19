77effa402733ad450a337accbe5c84166c9f194f
Riad Shash <riadshash@yahoo.com>
2020-02-18 19:06:45 -0700

    Initial commit

---
+++ Script.js
@@ -0,0 +1,11 @@
+function displayDate() {
+    document.getElementById('demo').innerHTML = Date();
+ }
+
+function addTwoNumbers(){
+    var num1, num2, result;
+    num1 = 1;
+    num2 = 1;
+    result = num1 + num2;
+    document.getElementById('demo').innerHTML = result;
+}
